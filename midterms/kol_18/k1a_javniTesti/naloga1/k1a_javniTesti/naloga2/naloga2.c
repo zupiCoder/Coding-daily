7c211ee252972fc61a299d9d144ee9e740b7cade
luka zupan <160914549+zupiCoder@users.noreply.github.com>
2025-02-24 18:36:51 +0100

    ...

--- midterms/kol_18/k1a_javniTesti/naloga1/k1a_javniTesti/naloga2/naloga2.c
+++ midterms/kol_18/k1a_javniTesti/naloga1/k1a_javniTesti/naloga2/naloga2.c
@@ -1,46 +1,64 @@
-
-/*
- * Va"sa re"sitev druge naloge --- datoteko dopolnite in jo oddajte na spletno u"cilnico!
- *
- * Your solution to task 1 --- complete the file and submit it to U"cilnica!
- *
- * V naslednjo vrstico vpi"site va"so vpisno "stevilko / Enter your student ID number in the next line:
- *
- *
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
 #include "naloga2.h"
 
-/*
- * Testni primeri J1--J2 in S1--S10 testirajo rezultat funkcije.
- * Testni primeri J3--J4 in S11--S20 testirajo vrednost spremenljivke, na
- *     katero ka"ze kazalec <indeks>.
- *
- * Test cases J1--J2 and S1--S10 check the result of the function.
- * Test cases J3--J4 and S11--S20 check the value of the variable pointed to
- *     by <indeks>.
- */
 int steviloInKta(char* niz, int k, int* indeks) {
-    // popravite oz. dopolnite / modify and/or add ...
-    return -1;
+    int temp = 0; bool found = false;
+    for(int i = 0; i < strlen(niz); i++) {
+        if('A' <= niz[i] && niz[i] <= 'Z') temp++;
+        if(temp == k && !found) {
+            *indeks = i;
+            found = true;
+        }
+    }
+    if(temp < k) *indeks = -1; 
+
+    return  temp;
 }
 
 void indeksi(char* niz, int** t) {
-    // dopolnite ... / complete ...
+    int indx = -1;
+    int st = steviloInKta(niz, 0, &indx);
+
+    int* arr = malloc((st+1) * sizeof(int));
+
+    int i = 0;
+    for(int k = st; k > 0; k--) {
+        int temp = -1; 
+        steviloInKta(niz, k, &temp);
+        arr[i] = temp;
+        i++;
+    }
+    arr[st] = -1;
+    *t = arr;
 }
 
 char** zadnje(char** nizi) {
-    // popravite oz. dopolnite / modify and/or add ...
-    return NULL;
+    char** temp = nizi;
+    int d = 0;
+    while(*temp != NULL) {
+        d++; temp++;
+    }
+    char** t = malloc(d * sizeof(char*));
+
+    for(int i = 0; i < d; i++) {
+        int indx = -1;
+        int st = steviloInKta(nizi[i], 0, &indx);
+        int* p = malloc(st * sizeof(int));
+
+        indeksi(nizi[i], &p);
+        if(p[0] == -1) {
+            t[i] = NULL;
+        } else {
+            t[i] = &nizi[i][p[0]];        
+        }
+    }
+    return t;
 }
 
 int main() {
-    // po "zelji dodajte kodo za ro"cno testiranje ...
-    // add manual testing code if desired ...
+
     return 0;
 }
