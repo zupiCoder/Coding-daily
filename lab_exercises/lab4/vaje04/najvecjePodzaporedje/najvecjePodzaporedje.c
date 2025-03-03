454bece9ae0952df8711acb49aa2aecb67c5a7c0
zupiCoder <zupi.inquiries@gmail.com>
2025-03-03 14:32:29 +0100

    implement recursive, try dp

--- lab_exercises/lab4/vaje04/najvecjePodzaporedje/najvecjePodzaporedje.c
+++ lab_exercises/lab4/vaje04/najvecjePodzaporedje/najvecjePodzaporedje.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <limits.h>
+
+//recursive too slow
+int recursive(int* t, int n, int i, int sum, int maxSum) {
+    
+    if(i == n) return maxSum > sum ? maxSum : sum;
+
+    int take = recursive(t, n, i+1, sum + t[i], maxSum);
+
+    int skip = recursive(t, n, i+1, 0, sum > maxSum ? sum : maxSum);
+
+    return take > skip ? take : skip;
+}
+
+int recursiveSetter(int* t, int n) {
+    return recursive(t, n, 0, 0, INT_MIN);
+}
     
 int main() {
 
@@ -9,16 +25,17 @@ int main() {
     scanf("%d", &n);
 
     int max_sum = INT_MIN; int temp_sum = 0;
-    
+    int* t = malloc(n * sizeof(int));
+
     for(int i = 0; i < n; i++) {
-        int input = 0; scanf("%d", &input);
-        temp_sum += input;
+        int input = 0; scanf("%d", &t[i]);
+        temp_sum += t[i];
 
         if(temp_sum > max_sum) max_sum = temp_sum;
         if(temp_sum < 0) temp_sum = 0;        
     }
 
-    printf("%d\n", max_sum);
+    printf("%d\n", recursiveSetter(t,n));
 
     return 0;
 }
