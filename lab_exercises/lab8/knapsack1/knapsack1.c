d3c1580ef66c605c8efc97db1d9fe9b82e48782b
zupiCoder <lz79481@student.uni-lj.si>
2025-04-17 21:53:30 +0200

    .

--- lab_exercises/lab8/knapsack1/knapsack1.c
+++ lab_exercises/lab8/knapsack1/knapsack1.c
@@ -6,7 +6,7 @@ int memo[2000][2000];//stores the maximum value you can get from i-th item to n-
 
 int knapsack(int V, int n, int* v, int* c, int i) {
     if(i == n) return 0;
-
+    
     int cena1 = 0, cena2 = 0;
 
     if(memo[i][V] != -1) return memo[i][V];
@@ -15,7 +15,7 @@ int knapsack(int V, int n, int* v, int* c, int i) {
     cena1 = c[i] + knapsack(V - v[i], n, v, c, i + 1);
     cena2 = knapsack(V, n, v, c, i + 1);
 
-    //calculate the cena1 if we dont take the item && cena2 if we take the item, then write the maximum into memo[i][V] where V is our current capacity
+    //calculate cena1 if we dont take the item && cena2 if we take the item, then write the maximum into memo[i][V] where V is our current capacity
     return memo[i][V] = cena1 > cena2 ? cena1 : cena2;
 }
 
@@ -33,7 +33,6 @@ int main() {
     for(int i = 0; i < 2000; i++) {
         for(int j = 0; j < 2000; j++) memo[i][j] = -1;
     }
-
     int max = knapsack(V, n, v, c, 0);
 
     printf("%d\n", max);
