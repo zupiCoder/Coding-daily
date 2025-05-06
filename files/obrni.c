e7f1d05ff7c4d53d6dc4e19d755b710d0a1b8a09
zupiCoder <lz79481@student.uni-lj.si>
2025-05-06 20:50:16 +0200

    exam task

---
+++ files/obrni.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 1000
+
+void read_file(FILE* vhod, FILE* izhod, char *buffer) {
+    char *temp = buffer;
+    int remaining_size = MAX_SIZE;
+
+    while(fgets(temp, remaining_size, vhod)) {
+        int len = strlen(temp);
+        temp += len;
+        remaining_size -= len;
+    }
+}
+
+void obrni(FILE* vhod, FILE* izhod) {
+    
+    char buffer[1000];
+    read_file(vhod, izhod, buffer);
+
+    int first_string = 0;
+    size_t start_index = strlen(buffer) - 1;
+
+    for(size_t index = start_index; index > 0; index--) {
+        if(buffer[index - 1] == '\n') {
+            char *temp = (buffer + index);
+            printf("%s", temp);
+
+            if(first_string == 0) {
+                first_string = 1;
+                printf("\n");
+            }
+
+            buffer[index] = '\0';
+        }
+    }
+    printf("%s", buffer);
+}
+
+int main(int argc, char *argv[]) {
+
+    FILE* vhod = fopen(argv[1], "r");
+    FILE* izhod = fopen(argv[2], "w");
+
+    obrni(vhod, izhod);
+
+    return 0;
+}
